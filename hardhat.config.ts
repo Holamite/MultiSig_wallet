b5644b12c97767f7d7dc050149a02b844f383110
Holamite <olamideadebara@gmail.com>
2024-02-20 18:49:11 +0100

    Successfully deployed and interacted

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
+const SEPOLIA_API_KEY_URL = process.env.SEPOLIA_API_KEY_URL || "";
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY || "";
+
+type HttpNetworkAccountsUserConfig = any;
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/FRC9i8y6nwyWBdY1i3ahDh68Eeoqh2tt",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
